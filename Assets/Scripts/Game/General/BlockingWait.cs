f25f29b2dca8e0b8a5b6fa0d002a2ca844f20e7e
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2023-09-16 17:45:34 +0300

    Blocking logic is added. Needed to add 5 day limit

--- Assets/Scripts/Game/General/BlockingWait.cs
+++ Assets/Scripts/Game/General/BlockingWait.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,20 +6,36 @@ public class BlockingWait : MonoBehaviour
 {
     [SerializeField]
     Slider sliderRef;
-
+    [SerializeField] private float waitTime;
+    private float remainingTime;
     [SerializeField]
     GameObject finishedPopup;
-
+    public Action OnBlockingDone;
     private void OnEnable()
     {
-        finishedPopup.SetActive(true);
         Debug.Log("BlockingWait:OnEnable");
+        remainingTime = waitTime;
+    }
+
+    private void Update()
+    {
+        if (remainingTime > 0)
+        {
+            sliderRef.value = remainingTime / waitTime;
+            remainingTime -= Time.deltaTime;
+            if (remainingTime<=0)
+            {
+                remainingTime = 0;
+                finishedPopup.SetActive(true);
+            }
+        }
     }
 
     public void CloseBlockingScreen()
     {
         finishedPopup.SetActive(false);
         gameObject.SetActive(false);
+        OnBlockingDone?.Invoke();
         Debug.Log("BlockingWait:CloseBlockingScreen");
     }
 }
--- Assets/Scripts/Game/General/MainMenu.cs
+++ Assets/Scripts/Game/General/MainMenu.cs
@@ -1,14 +1,24 @@
 using CurrencySystem;
 using Game.TimeSystem;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private BlockingWait blockingWaitPopup;
     [SerializeField] private CurrencyController currencyController;
     [SerializeField] private SavableDailyEvent savableDailyEvent;
-    
+
+
+    private void OnEnable()
+    {
+        blockingWaitPopup.OnBlockingDone+=OnBlockingDone;
+    }
+
+    private void OnDisable()
+    {
+        blockingWaitPopup.OnBlockingDone-=OnBlockingDone;
+    }
+
     public void Action_SpendOneCoin()
     {
         currencyController.Decrease(1);
@@ -17,6 +27,10 @@ public class MainMenu : MonoBehaviour
     public void Action_GetExtraCoin()
     {
         blockingWaitPopup.gameObject.SetActive(true);
+    }
+
+    void OnBlockingDone()
+    {
         currencyController.Increase(1);
     }
 
